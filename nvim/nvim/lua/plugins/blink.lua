65b50b025869dcd74d9f7b194ccffdb5c4dac6ba
Keynold <nqphu1998@gmail.com>
2025-03-16 06:27:22 +0700

    feat: update blink lua

--- nvim/nvim/lua/plugins/blink.lua
+++ nvim/nvim/lua/plugins/blink.lua
@@ -12,8 +12,10 @@ return {
       signature = { enabled = true },
       keymap = { preset = "enter" },
       completion = {
+        keyword = { range = "prefix" },
         accept = { auto_brackets = { enabled = false } },
-        ghost_text = { enabled = true },
+        ghost_text = { enabled = false },
+        list = { selection = { preselect = false, auto_insert = false } },
         menu = {
           auto_show = true,
           draw = {
@@ -21,19 +23,39 @@ return {
               { "label", "label_description", gap = 1 },
               { "kind_icon", "kind" },
             },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
           },
         },
       },
       sources = {
+        default = {
+          "lsp",
+          -- "path",
+          -- "snippets",
+          -- "buffer",
+        },
         providers = {
           lsp = {
             score_offset = 150,
-          }
-        }
-      }
+          },
+        },
+      },
       -- enable completion sources
       -- 'path' for file system paths
     },
-    opts_extend = { "sources.completion.enabled_providers" },
+    opts_extend = {
+      "sources.completion.enabled_providers",
+      "sources.compat",
+      "sources.default",
+    },
   },
 }
